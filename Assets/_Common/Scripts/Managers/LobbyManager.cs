85e7273593f159417255c154c0f407b624d8777d
Song <awesomeday322@gmail.com>
2026-01-25 15:29:00 +0900

    perf: 빌드 버전 최적화(디버그 로그 비활성화)

--- Assets/_Common/Scripts/Managers/LobbyManager.cs
+++ Assets/_Common/Scripts/Managers/LobbyManager.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/* ======================================================================
+ * [Class]: LobbyManager 
+ * [Role] : 로비 씬에서 게임 씬으로 이동 및 초기 설정(모든 스크립트 내의 로그를 Off)을 담당
+ * ====================================================================== */
 public class LobbyManager : MonoBehaviour
 {
+
+    void Awake()
+    {
+#if !UNITY_EDITOR
+        Debug.unityLogger.logEnabled = false;
+#endif
+    }
     public void LoadGameScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
+
 }
