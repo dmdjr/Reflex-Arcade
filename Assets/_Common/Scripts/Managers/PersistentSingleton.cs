284b0ac16293159e2dbaad49b8920514ed2aaca2
Song <awesomeday322@gmail.com>
2026-04-21 13:54:57 +0900

    feat: Mirror Dodge 로비 씬 UI 업데이트

--- Assets/_Common/Scripts/Managers/PersistentSingleton.cs
+++ Assets/_Common/Scripts/Managers/PersistentSingleton.cs
@@ -14,7 +14,9 @@ public abstract class PersistentSingleton<T> : MonoBehaviour where T : MonoBehav
         if (Instance == null)
         {
             Instance = this as T;
-            DontDestroyOnLoad(gameObject);
+            // 자식 오브젝트에서 호출 시에도 동작하도록 루트 오브젝트에 적용
+            // (@Managers 같은 부모 오브젝트 아래 묶어도 정상 작동)
+            DontDestroyOnLoad(transform.root.gameObject);
         }
         else
         {
